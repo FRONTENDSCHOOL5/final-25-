b94be8c79228e93fb6787258897fd9492910aff1
kimjiu <jiwoo2985@naver.com>
2023-06-14 00:59:51 +0900

    [🎨style] 팔로워페이지 class 수정 #44

--- src/pages/Profile/Followers.jsx
+++ src/pages/Profile/Followers.jsx
@@ -8,15 +8,16 @@ export default function Followers() {
       <header className={styles['header-followers']}>Followers</header>
       <div className={styles.followers}>
         <div className={styles['followers-photo']}></div>
-        <div className={styles['followers-inner']}>
-          <span className={styles['followers-name']}>
-            애월읍 한라봉 최고 맛집
-          </span>
+        {/* <div className={styles['followers-inner']}> */}
+        <div
+          className={`${styles['followers-inner']} ${styles['followers-name']}`}
+        >
+          애월읍 한라봉 최고 맛집
         </div>
-        <div className={styles['followers-inner']}>
-          <span className={styles['followers-info']}>
-            정성을 다해 농사 짓는 한라봉
-          </span>
+        <div
+          className={`${styles['followers-inner']} ${styles['followers-info']}`}
+        >
+          정성을 다해 농사 짓는 한라봉
         </div>
         <button type="button" className={styles['followers-btn-follow']}>
           팔로우
@@ -24,13 +25,17 @@ export default function Followers() {
       </div>
       <div className={styles.followers}>
         <div className={styles['followers-photo']}></div>
-        <div className={styles['followers-inner']}>
-          <span className={styles['followers-name']}>감귤의 품격 - 애월읍</span>
+        <div
+          className={`${styles['followers-inner']} ${styles['followers-name']}`}
+        >
+          감귤의 품격 - 애월읍
         </div>
         <div className={styles['followers-inner']}>
-          <span className={styles['followers-info']}>
+          <div
+            className={`${styles['followers-inner']} ${styles['followers-info']}`}
+          >
             제주 노지귤, 하우스 한라봉 판매합니다라마바사아자
-          </span>
+          </div>
         </div>
         <button type="button" className={styles['followers-btn-follow']}>
           팔로우
@@ -38,13 +43,17 @@ export default function Followers() {
       </div>
       <div className={styles.followers}>
         <div className={styles['followers-photo']}></div>
-        <div className={styles['followers-inner']}>
-          <span className={styles['followers-name']}>한라봉의 신</span>
+        <div
+          className={`${styles['followers-inner']} ${styles['followers-name']}`}
+        >
+          한라봉의 신
         </div>
         <div className={styles['followers-inner']}>
-          <span className={styles['followers-info']}>
+          <div
+            className={`${styles['followers-inner']} ${styles['followers-info']}`}
+          >
             30년 노하우로 정성스럽게 농사지은 노지 귤 판매합니다
-          </span>
+          </div>
         </div>
         <button type="button" className={styles['followers-btn-unfollow']}>
           취소
