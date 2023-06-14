839656eaa15868d848489424b4f90c174381d50b
jsunbin <jsnb6n@gmail.com>
2023-06-14 14:51:51 +0900

    [feat] 홈, 로그인, 프로필, 챗팅 라우팅 연결

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
-import ProfileProduct from './components/common/Profile/ProfileProduct';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import './styles/reset.css';
+import Feed from './pages/Feed/Feed';
+import LoginMain from './pages/Login/LoginMain/LoginMain';
+import MyProfile from './pages/Profile/MyProfile/MyProfile';
+import ChatList from './pages/Chat/ChatList/ChatList';
 
 export default function App() {
-  return <ProfileProduct />;
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Feed />}></Route>
+          <Route path="/login" element={<LoginMain />}></Route>
+          <Route path="/profile" element={<MyProfile />}></Route>
+          <Route path="/chat" element={<ChatList />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
