170229f87d21a11994e6c84bdb41e64d7ae27620
jsunbin <jsnb6n@gmail.com>
2023-06-14 22:36:06 +0900

    [feat] 회원가입 라우팅 연결

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './styles/reset.css';
 import Feed from './pages/Feed/Feed';
 import LoginMain from './pages/Login/LoginMain/LoginMain';
+import Join from './pages/Join/JoinEmail/JoinEmail';
 import MyProfile from './pages/Profile/MyProfile/MyProfile';
 import ChatList from './pages/Chat/ChatList/ChatList';
 
@@ -12,6 +13,7 @@ export default function App() {
         <Routes>
           <Route path="/" element={<Feed />}></Route>
           <Route path="/login" element={<LoginMain />}></Route>
+          <Route path="/join" element={<Join />}></Route>
           <Route path="/profile" element={<MyProfile />}></Route>
           <Route path="/chat" element={<ChatList />}></Route>
         </Routes>
