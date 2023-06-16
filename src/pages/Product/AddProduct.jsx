fca792fceda14ca053f2b3078122e557eea26d70
ShinKiChan <wkr00@naver.com>
2023-06-16 20:33:04 +0900

    [style]  main, email,search,product 수정 #61

--- src/pages/Product/AddProduct.jsx
+++ src/pages/Product/AddProduct.jsx
@@ -1,59 +1,52 @@
 import React from 'react';
 import styles from './AddProduct.module.css';
-import BackIcon from '../../assets/images/icon-arrow-left.svg';
+import SaveButton from '../../components/common/Header/SaveButton';
 
 export default function AddProduct() {
   return (
     <>
-      <section className={styles['ProductWrapper']}>
-        <article className={styles['ProductMain']}>
-          <div className={styles['ProductHeader']}>
-            <div className={styles['ProductArrow']}>
-              <a href="/">
-                <img
-                  className={styles['BackIcon']}
-                  src={BackIcon}
-                  alt="뒤로가기 아이콘 입니다."
-                />
-              </a>
-            </div>
-
-            <button className={styles['ProductBtn']}>저장</button>
-          </div>
-          <div className={styles['ProductBody']}>
-            <div className={styles['ProductImageBox']}>
-              <div>이미지 등록</div>
-              <label className={styles['fileLabel']} for="Input"></label>
-              <input className={styles['fileInput']} type="file" id="Input" />
-            </div>
-
-            <div className={styles['ProductTitle']}>
-              <div>공구 이름</div>
-              <input
-                className={styles['ProductTitleInput']}
-                type="text"
-                placeholder="2~15자 이내여야 합니다."
-              />
-            </div>
-            <div className={styles['ProductTitle']}>
-              <div>가격</div>
-              <input
-                className={styles['ProductTitleInput']}
-                type="number"
-                placeholder="숫자만 입력 가능합니다."
-              />
-            </div>
-            <div className={styles['ProductTitle']}>
-              <div>판매링크</div>
-              <input
-                className={styles['ProductTitleInput']}
-                type="text"
-                placeholder="URL을 입력해 주세요"
-              />
-            </div>
-          </div>
-        </article>
-      </section>
+      <header className={styles['product-header']}>
+        <SaveButton />
+      </header>
+      <main className={styles['product-main']}>
+        <section className={styles['product-image-container']}>
+          <div>이미지 등록</div>
+          <form action="">
+            <label
+              className={styles['product-file-upload']}
+              for="Input"
+            ></label>
+            <input className={styles['a11y-hidden']} type="file" id="Input" />
+          </form>
+        </section>
+        <section className={styles['product-title']}>
+          <div>공구 이름</div>
+          <input
+            className={styles['product-title-input']}
+            type="text"
+            placeholder="2~15자 이내여야 합니다."
+            required
+          />
+        </section>
+        <section className={styles['product-title']}>
+          <div>가격</div>
+          <input
+            className={styles['product-title-input']}
+            type="number"
+            placeholder="숫자만 입력 가능합니다."
+            required
+          />
+        </section>
+        <section className={styles['product-title']}>
+          <div>판매링크</div>
+          <input
+            className={styles['product-title-input']}
+            type="text"
+            placeholder="URL을 입력해 주세요"
+            required
+          />
+        </section>
+      </main>
     </>
   );
 }
--- src/pages/SearchUser/SearchUser.jsx
+++ src/pages/SearchUser/SearchUser.jsx
@@ -1,104 +1,23 @@
 import React from 'react';
 import styles from './SearchUser.module.css';
-import BackIcon from '../../assets/images/icon-arrow-left.svg';
-import defaultProfileImg from '../../assets/images/profile-img42.png';
-
+import Search from '../../components/common/Header/UserSearch';
+import UserSearch from '../../components/common/User/UserSearch/UserSearch';
+import TabMenu from '../../components/common/TabMenu/TabMenu';
 export default function SearchUser() {
   return (
     <>
-      <section className={styles['SearchWrapper']}>
-        <div className={styles['ProductHeader']}>
-          <div className={styles['ProductArrow']}>
-            <a href="/">
-              <img
-                className={styles['BackIcon']}
-                src={BackIcon}
-                alt="뒤로가기 아이콘 입니다."
-              />
-            </a>
-          </div>
-          <div className={styles['SearchContain']}>
-            <input
-              className={styles['SearchInput']}
-              type="text"
-              placeholder="계정 검색"
-            />
-          </div>
-        </div>
-
-        <div className={`${styles['user']} ${styles['user-search']}`}>
-          <h2 className={styles['a11y-hidden']}>사용자 계정</h2>
-          <img
-            className={styles['profile-cover']}
-            src={defaultProfileImg}
-            alt="프로필 이미지"
-          />
-          <div className={styles['user-info']}>
-            <h3 className={styles['user-name']}>
-              <span className={styles['user-strong']}>애월읍 </span>위니브
-              감귤농장
-            </h3>
-            <div className={styles['user-id']}>@ weniv_Mandarin</div>
-          </div>
-        </div>
-        <div className={`${styles['user']} ${styles['user-search']}`}>
-          <h2 className={styles['a11y-hidden']}>사용자 계정</h2>
-          <img
-            className={styles['profile-cover']}
-            src={defaultProfileImg}
-            alt="프로필 이미지"
-          />
-          <div className={styles['user-info']}>
-            <h3 className={styles['user-name']}>
-              <span className={styles['user-strong']}>애월읍 </span>한라봉
-              최고맛집
-            </h3>
-            <div className={styles['user-id']}>@ weniv_Mandarin</div>
-          </div>
-        </div>
-        <div className={`${styles['user']} ${styles['user-search']}`}>
-          <h2 className={styles['a11y-hidden']}>사용자 계정</h2>
-          <img
-            className={styles['profile-cover']}
-            src={defaultProfileImg}
-            alt="프로필 이미지"
-          />
-          <div className={styles['user-info']}>
-            <h3 className={styles['user-name']}>
-              감귤의 품격 -
-              <span className={styles['user-strong']}> 애월읍 </span>
-            </h3>
-            <div className={styles['user-id']}>@ weniv_Mandarin</div>
-          </div>
-        </div>
-
-        <footer>
-          <nav className={styles['nav-bar']}>
-            <ul className={styles['menu-list']}>
-              <li>
-                <a href="/" className={styles['menu-home']}>
-                  홈
-                </a>
-              </li>
-              <li>
-                <a href="/chat" className={styles['menu-chat']}>
-                  채팅
-                </a>
-              </li>
-              <li>
-                <a href="/upload" className={styles['menu-post']}>
-                  게시물 작성
-                </a>
-              </li>
-              <li>
-                <a href="/profile" className={styles['menu-profile']}>
-                  프로필
-                </a>
-              </li>
-            </ul>
-          </nav>
-        </footer>
-      </section>
+      <header className={styles['search-header']}>
+        <Search />
+      </header>
+      <main className={styles['search-main']}>
+        <UserSearch />
+        <UserSearch />
+        <UserSearch />
+        <UserSearch />
+      </main>
+      <footer className={styles['search-footer']}>
+        <TabMenu />
+      </footer>
     </>
   );
 }
