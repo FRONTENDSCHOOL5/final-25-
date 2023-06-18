2f6b1d9fbec10d9ce5a699e4512354bebbed3439
kimjiu <jiwoo2985@naver.com>
2023-06-18 23:46:18 +0900

    [🎨style] Modal과 Page 코드 수정

--- src/components/common/Modal/PostModal.jsx
+++ src/components/common/Modal/PostModal.jsx
@@ -4,6 +4,7 @@ import styles from './PostModal.module.css';
 export default function PostModal() {
   return (
     <section>
+      <h1 className="a11y-hidden">채팅방나가기창</h1>
       <div className={styles['alert-post']}>
         <button type="button" className={styles['btn-out']}>
           채팅방 나가기
