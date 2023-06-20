9059406d5ca2aef2001570e3763efe282800bbed
jsunbin <jsnb6n@gmail.com>
2023-06-20 15:34:52 +0900

    [feat] 리스트 또는 앨범으로 보기 버튼에 기능 추가

--- src/components/common/Profile/ProfilePost.jsx
+++ src/components/common/Profile/ProfilePost.jsx
@@ -1,22 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Post from '../Post/Post';
 import styles from './ProfilePost.module.css';
 import postListOn from '../../../assets/images/icon-post-list-on.svg';
+import postListOff from '../../../assets/images/icon-post-list-off.svg';
+import postAlbumOn from '../../../assets/images/icon-post-album-on.svg';
 import postAlbumOff from '../../../assets/images/icon-post-album-off.svg';
 
-export default function ProfilePost() {
-  let type;
-  switch (document.location.pathname) {
-    case '/':
-      type = 'feed';
-      break;
-    case '/post':
-      type = 'post';
-      break;
-    default:
-      type = 'none';
-      break;
-  }
+export default function ProfilePost({ type }) {
+  // false가 리스트로 보기
+  // true가 앨범으로 보기
+  const [option, setOption] = useState('리스트로 보기');
+  const [isOptionClicked, setIsOptionClicked] = useState(false);
+  const optionHandler = event => {
+    if (event.target.alt !== option) {
+      if (event.target.alt === '앨범으로 보기') {
+        setOption('앨범으로 보기');
+        setIsOptionClicked(true);
+      } else {
+        setOption('리스트로 보기');
+        setIsOptionClicked(false);
+      }
+    }
+  };
 
   const ProfilePostUI = {
     feed: (
@@ -35,19 +40,55 @@ export default function ProfilePost() {
     ),
     profile: (
       <section className={styles.profile}>
-        <div className={styles['btn-group']}>
-          <button type="button" className={styles['btn-list']}>
-            <img src={postListOn} alt="리스트로 보기" />
-          </button>
-          <button type="button" className={styles['btn-album']}>
-            <img src={postAlbumOff} alt="앨범으로 보기" />
-          </button>
-        </div>
-        <ul className={styles['post-list']}>
-          <li>
-            <Post />
-          </li>
-        </ul>
+        {isOptionClicked ? (
+          <>
+            <div className={styles['btn-group']}>
+              <button
+                type="button"
+                className={styles['btn-list']}
+                onClick={optionHandler}
+              >
+                <img src={postListOff} alt="리스트로 보기" />
+              </button>
+              <button
+                type="button"
+                className={styles['btn-album']}
+                onClick={optionHandler}
+              >
+                <img src={postAlbumOn} alt="앨범으로 보기" />
+              </button>
+            </div>
+            <ul className={styles['post-album']}>
+              <li className={styles['post-album-item']}>
+                <img src="https://picsum.photos/200" alt="포스트 썸네일" />
+              </li>
+            </ul>
+          </>
+        ) : (
+          <>
+            <div className={styles['btn-group']}>
+              <button
+                type="button"
+                className={styles['btn-list']}
+                onClick={optionHandler}
+              >
+                <img src={postListOn} alt="리스트로 보기" />
+              </button>
+              <button
+                type="button"
+                className={styles['btn-album']}
+                onClick={optionHandler}
+              >
+                <img src={postAlbumOff} alt="앨범으로 보기" />
+              </button>
+            </div>
+            <ul className={styles['post-list']}>
+              <li>
+                <Post />
+              </li>
+            </ul>
+          </>
+        )}
       </section>
     ),
   };
