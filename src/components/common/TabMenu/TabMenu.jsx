0ddc6d9853f500f9c622fac391e020cf832597ec
jsunbin <jsnb6n@gmail.com>
2023-06-20 16:18:38 +0900

    [design] Footer border-top 추가 #102

--- src/components/common/TabMenu/TabMenu.jsx
+++ src/components/common/TabMenu/TabMenu.jsx
@@ -5,120 +5,128 @@ export default function TabMenu({ type }) {
   const TabMenuUI = {
     none: <></>,
     home: (
-      <nav className={styles['nav-bar']}>
-        <ul className={styles['menu-list']}>
-          <li>
-            <a
-              href="/"
-              className={`${styles['menu-home']} ${styles['active']}`}
-            >
-              홈
-            </a>
-          </li>
-          <li>
-            <a href="/chat" className={styles['menu-chat']}>
-              채팅
-            </a>
-          </li>
-          <li>
-            <a href="/post" className={styles['menu-post']}>
-              게시물 작성
-            </a>
-          </li>
-          <li>
-            <a href="/profile" className={styles['menu-profile']}>
-              프로필
-            </a>
-          </li>
-        </ul>
-      </nav>
+      <footer className={styles['footer']}>
+        <nav className={styles['nav-bar']}>
+          <ul className={styles['menu-list']}>
+            <li>
+              <a
+                href="/"
+                className={`${styles['menu-home']} ${styles['active']}`}
+              >
+                홈
+              </a>
+            </li>
+            <li>
+              <a href="/chat" className={styles['menu-chat']}>
+                채팅
+              </a>
+            </li>
+            <li>
+              <a href="/post" className={styles['menu-post']}>
+                게시물 작성
+              </a>
+            </li>
+            <li>
+              <a href="/profile" className={styles['menu-profile']}>
+                프로필
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </footer>
     ),
     chat: (
-      <nav className={styles['nav-bar']}>
-        <ul className={styles['menu-list']}>
-          <li>
-            <a href="/" className={styles['menu-home']}>
-              홈
-            </a>
-          </li>
-          <li>
-            <a
-              href="/chat"
-              className={`${styles['menu-chat']} ${styles['active']}`}
-            >
-              채팅
-            </a>
-          </li>
-          <li>
-            <a href="/post" className={styles['menu-post']}>
-              게시물 작성
-            </a>
-          </li>
-          <li>
-            <a href="/profile" className={styles['menu-profile']}>
-              프로필
-            </a>
-          </li>
-        </ul>
-      </nav>
+      <footer className={styles['footer']}>
+        <nav className={styles['nav-bar']}>
+          <ul className={styles['menu-list']}>
+            <li>
+              <a href="/" className={styles['menu-home']}>
+                홈
+              </a>
+            </li>
+            <li>
+              <a
+                href="/chat"
+                className={`${styles['menu-chat']} ${styles['active']}`}
+              >
+                채팅
+              </a>
+            </li>
+            <li>
+              <a href="/post" className={styles['menu-post']}>
+                게시물 작성
+              </a>
+            </li>
+            <li>
+              <a href="/profile" className={styles['menu-profile']}>
+                프로필
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </footer>
     ),
     post: (
-      <nav className={styles['nav-bar']}>
-        <ul className={styles['menu-list']}>
-          <li>
-            <a href="/" className={styles['menu-home']}>
-              홈
-            </a>
-          </li>
-          <li>
-            <a href="/chat" className={styles['menu-chat']}>
-              채팅
-            </a>
-          </li>
-          <li>
-            <a
-              href="/post"
-              className={`${styles['menu-post']} ${styles['active']}`}
-            >
-              게시물 작성
-            </a>
-          </li>
-          <li>
-            <a href="/profile" className={styles['menu-profile']}>
-              프로필
-            </a>
-          </li>
-        </ul>
-      </nav>
+      <footer className={styles['footer']}>
+        <nav className={styles['nav-bar']}>
+          <ul className={styles['menu-list']}>
+            <li>
+              <a href="/" className={styles['menu-home']}>
+                홈
+              </a>
+            </li>
+            <li>
+              <a href="/chat" className={styles['menu-chat']}>
+                채팅
+              </a>
+            </li>
+            <li>
+              <a
+                href="/post"
+                className={`${styles['menu-post']} ${styles['active']}`}
+              >
+                게시물 작성
+              </a>
+            </li>
+            <li>
+              <a href="/profile" className={styles['menu-profile']}>
+                프로필
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </footer>
     ),
     profile: (
-      <nav className={styles['nav-bar']}>
-        <ul className={styles['menu-list']}>
-          <li>
-            <a href="/" className={styles['menu-home']}>
-              홈
-            </a>
-          </li>
-          <li>
-            <a href="/chat" className={styles['menu-chat']}>
-              채팅
-            </a>
-          </li>
-          <li>
-            <a href="/post" className={styles['menu-post']}>
-              게시물 작성
-            </a>
-          </li>
-          <li>
-            <a
-              href="/profile"
-              className={`${styles['menu-profile']} ${styles['active']}`}
-            >
-              프로필
-            </a>
-          </li>
-        </ul>
-      </nav>
+      <footer className={styles['footer']}>
+        <nav className={styles['nav-bar']}>
+          <ul className={styles['menu-list']}>
+            <li>
+              <a href="/" className={styles['menu-home']}>
+                홈
+              </a>
+            </li>
+            <li>
+              <a href="/chat" className={styles['menu-chat']}>
+                채팅
+              </a>
+            </li>
+            <li>
+              <a href="/post" className={styles['menu-post']}>
+                게시물 작성
+              </a>
+            </li>
+            <li>
+              <a
+                href="/profile"
+                className={`${styles['menu-profile']} ${styles['active']}`}
+              >
+                프로필
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </footer>
     ),
   };
   return TabMenuUI[type];
