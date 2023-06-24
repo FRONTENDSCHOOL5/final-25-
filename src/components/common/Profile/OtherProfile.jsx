6e7e600241e1a4a7cdae2aaf1fc60560ab3cb305
jsunbin <jsnb6n@gmail.com>
2023-06-24 17:09:54 +0900

    [feat] 유저 프로필 정보 API 연결 및 프로필 이미지 스타일 수정 #134

--- src/components/common/Profile/OtherProfile.jsx
+++ src/components/common/Profile/OtherProfile.jsx
@@ -1,33 +1,75 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from './OtherProfile.module.css';
 import basicProfileImg from '../../../assets/images/basic-profile-img.png';
 
 export default function UserProfile() {
+  const token = localStorage.getItem('token');
+  const userAccountName = document.location.pathname.replace('/profile/', '');
+  const [profileInfo, setProfileInfo] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  console.log(userAccountName);
+
+  useEffect(() => {
+    async function fetchProfile() {
+      setIsLoading(true);
+      try {
+        const response = await fetch(
+          `https://api.mandarin.weniv.co.kr/profile/${userAccountName}`,
+          {
+            method: 'GET',
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          },
+        );
+
+        if (!response.ok) {
+          throw new Error('네트워크에 문제가 있습니다!');
+        }
+        const data = await response.json();
+        console.log('다른계정:', data);
+        setProfileInfo(data['profile']);
+        setIsLoading(false);
+      } catch (error) {
+        console.error(error);
+        setIsLoading(false);
+      }
+    }
+    fetchProfile();
+  }, []);
+
   return (
-    <>
+    !isLoading && (
       <section className={styles['user-profile']}>
         <img
           className={styles['user-profile-cover']}
-          src={basicProfileImg}
+          src={profileInfo['image']}
           alt="프로필 사진"
         />
         <div className={styles['user-profile-info']}>
           <strong className={styles['user-profile-name']}>
-            이오에서만은 디자인왕
+            {profileInfo['username']}
           </strong>
-          <span className={styles['user-profile-id']}>@ e5_designKing99</span>
+          <span className={styles['user-profile-id']}>
+            @ {profileInfo['accountname']}
+          </span>
           <span className={styles['user-profile-intro']}>
-            애월읍 감귤 전국 배송, 귤따기 체험, 감귤 농장
+            {profileInfo['intro']}
           </span>
         </div>
         <div className={styles['user-count']}>
           <button type="button" className={styles['btn-followers']}>
             <span className={styles['followers']}>followers</span>
-            <span className={styles['followers-number']}>2950</span>
+            <span className={styles['followers-number']}>
+              {profileInfo['followerCount']}
+            </span>
           </button>
           <div className={styles['following-area']}>
             <span className={styles['followings']}>followings</span>
-            <span className={styles['followings-number']}>128</span>
+            <span className={styles['followings-number']}>
+              {profileInfo['followingCount']}
+            </span>
           </div>
         </div>
         <div className={styles['button-container']}>
@@ -37,11 +79,11 @@ export default function UserProfile() {
           <button className={styles['btn-follow']} type="button">
             팔로우
           </button>
-          <a className={styles['btn-share']} href="./">
+          <div className={styles['btn-share']}>
             <span className="a11y-hidden">공유하기</span>
-          </a>
+          </div>
         </div>
       </section>
-    </>
+    )
   );
 }
