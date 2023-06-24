4e56cdc4a90c45cc5e02827c6971323713631865
jongmi jung <whdal2636@naver.com>
2023-06-25 00:09:31 +0900

    [feat] 팔로워 페이지 api연동

--- src/pages/Profile/Followers.jsx
+++ src/pages/Profile/Followers.jsx
@@ -1,38 +1,38 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import styles from './Followers.module.css';
 import Layout from '../../components/layout/Layout';
+import { AuthContext } from '../../context/AuthContext';
+import profileAPI from '../../api/profileAPI';
 
 export default function Followers() {
   const FollowersComponent = () => {
-    const articles = [
-      {
-        id: 1,
-        name: '애월읍 한라봉 최고 맛집',
-        info: '정성을 다해 농사 짓는 한라봉',
-      },
-      {
-        id: 2,
-        name: '감귤의 품격 - 애월읍',
-        info: '제주 노지귤, 하우스 한라봉 판매합니다라마바사아자',
-      },
-      {
-        id: 3,
-        name: '한라봉의 신',
-        info: '30년 노하우로 정성스럽게 농사지은 노지 귤 판매합니다',
-      },
-    ];
+    const [followers, setFollowers] = useState([]);
+    const [buttonText, setButtonText] = useState({});
+    const [buttonClass, setButtonClass] = useState({});
 
-    const [buttonText, setButtonText] = useState({
-      1: '팔로우',
-      2: '팔로우',
-      3: '취소',
-    });
+    useEffect(() => {
+      // API 호출하여 팔로워 목록 가져오기
+      const fetchFollowers = async () => {
+        try {
+          const response = await profileAPI.getFollowers();
+          setFollowers(response); // 팔로워 목록을 상태로 설정
 
-    const [buttonClass, setButtonClass] = useState({
-      1: styles['followers-btn-follow'],
-      2: styles['followers-btn-follow'],
-      3: styles['followers-btn-unfollow'],
-    });
+          // 각 팔로워의 초기 버튼 상태 설정
+          const initialButtonText = {};
+          const initialButtonClass = {};
+          response.forEach(follower => {
+            initialButtonText[follower.id] = '팔로우';
+            initialButtonClass[follower.id] = styles['followers-btn-follow'];
+          });
+          setButtonText(initialButtonText);
+          setButtonClass(initialButtonClass);
+        } catch (error) {
+          console.error('Error fetching followers:', error);
+        }
+      };
+
+      fetchFollowers();
+    }, []);
 
     const updateButtonState = id => {
       setButtonText(prevText => ({
@@ -53,27 +53,27 @@ export default function Followers() {
       <Layout>
         <h2 className="a11y-hidden">팔로워목록</h2>
         <section className={styles['followers-list']}>
-          {articles.map(article => (
-            <article key={article.id} className={styles.followers}>
+          {followers.map(follower => (
+            <article key={follower.id} className={styles.followers}>
               <div className={styles['followers-photo']}></div>
               <p
                 className={`${styles['followers-inner']} ${styles['followers-name']}`}
               >
-                {article.name}
+                {follower.name}
               </p>
               <p
                 className={`${styles['followers-inner']} ${styles['followers-info']}`}
               >
-                {article.info}
+                {follower.info}
               </p>
               <button
                 type="button"
                 className={`${styles['followers-btn']} ${
-                  buttonClass[article.id]
+                  buttonClass[follower.id]
                 }`}
-                onClick={() => updateButtonState(article.id)}
+                onClick={() => updateButtonState(follower.id)}
               >
-                {buttonText[article.id]}
+                {buttonText[follower.id]}
               </button>
             </article>
           ))}
