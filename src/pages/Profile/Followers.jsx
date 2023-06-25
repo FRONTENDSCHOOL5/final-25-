3cfee8a4871e9f2970148109c4fa067fe5bc4e3b
jongmi jung <whdal2636@naver.com>
2023-06-25 18:33:49 +0900

    [fix] 팔로우 버튼 동시 변경되는 버그 수정(버튼 상태 객체에서 배열로 변경)

--- src/pages/Profile/Followers.jsx
+++ src/pages/Profile/Followers.jsx
@@ -7,8 +7,7 @@ import profileAPI from '../../api/profileAPI2';
 
 export default function Followers() {
   const [followers, setFollowers] = useState([]);
-  const [buttonText, setButtonText] = useState({});
-  const [buttonClass, setButtonClass] = useState({});
+  const [buttonStates, setButtonStates] = useState([]);
   const { user } = useContext(AuthContext);
   const { accountname } = useParams();
 
@@ -20,17 +19,14 @@ export default function Followers() {
           accountname,
         );
         setFollowers(response);
-        console.log('response데이터 확인 : ', response);
+        console.log('response 데이터 확인:', response);
 
         // 각 팔로워의 초기 버튼 상태 설정
-        const initialButtonText = {};
-        const initialButtonClass = {};
-        response.forEach(follower => {
-          initialButtonText[follower.id] = '팔로우';
-          initialButtonClass[follower.id] = styles['followers-btn-follow'];
+        const initialButtonStates = new Array(response.length).fill({
+          text: '팔로우',
+          className: styles['followers-btn-follow'],
         });
-        setButtonText(initialButtonText);
-        setButtonClass(initialButtonClass);
+        setButtonStates(initialButtonStates);
       } catch (error) {
         console.error('Error fetching followers:', error);
       }
@@ -41,29 +37,32 @@ export default function Followers() {
     }
   }, [followers.length, accountname, user.token]);
 
-  const updateButtonState = id => {
-    console.log('이건뭐지', id);
-    setButtonText(prevText => ({
-      ...prevText,
-      [id]: prevText[id] === '팔로우' ? '취소' : '팔로우',
-    }));
-
-    setButtonClass(prevClass => ({
-      ...prevClass,
-      [id]:
-        prevClass[id] === styles['followers-btn-follow']
-          ? styles['followers-btn-unfollow']
-          : styles['followers-btn-follow'],
-    }));
+  const updateButtonState = index => {
+    setButtonStates(prevStates => {
+      const updatedStates = [...prevStates];
+      updatedStates[index] = {
+        text: prevStates[index].text === '팔로우' ? '취소' : '팔로우',
+        className:
+          prevStates[index].className === styles['followers-btn-follow']
+            ? styles['followers-btn-unfollow']
+            : styles['followers-btn-follow'],
+      };
+      return updatedStates;
+    });
   };
 
   return (
     <Layout>
-      <h2 className="a11y-hidden">팔로워목록</h2>
+      <h2 className="a11y-hidden">팔로워 목록</h2>
       <section className={styles['followers-list']}>
         {followers.length > 0 ? (
-          followers.map(follower => (
-            <article key={follower.id} className={styles.followers}>
+          followers.map((follower, index) => (
+            <article
+              key={index}
+              index={index}
+              data={follower}
+              className={styles.followers}
+            >
               <div className={styles['followers-photo']}>
                 <img
                   src={follower.image}
@@ -83,12 +82,11 @@ export default function Followers() {
               </p>
               <button
                 type="button"
-                className={`${styles['followers-btn']} ${
-                  buttonClass[follower.id]
-                }`}
-                onClick={() => updateButtonState(follower.id)}
+                id={`btn-${index}`}
+                className={`${styles['followers-btn']} ${buttonStates[index]?.className}`}
+                onClick={() => updateButtonState(index)}
               >
-                {buttonText[follower.id]}
+                {buttonStates[index]?.text}
               </button>
             </article>
           ))
