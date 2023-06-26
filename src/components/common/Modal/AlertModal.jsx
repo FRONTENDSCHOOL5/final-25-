25c35126a2a044e5c319d089a87e41817a058384
jsunbin <jsnb6n@gmail.com>
2023-06-26 15:08:10 +0900

    [design] AlertModal UI 작업 #146

--- src/components/common/Modal/AlertModal.jsx
+++ src/components/common/Modal/AlertModal.jsx
@@ -1,20 +1,38 @@
 import React from 'react';
 import styles from './AlertModal.module.css';
 
-export default function AlertModal() {
-  return (
-    <section className={styles.alert}>
-      <div className={styles['alert-inner']}>
-        <h2 className={styles['alert-title']}>상품을 삭제할까요?</h2>
-        <div className={styles['btn-wrapper']}>
-          <button type="button" className={styles['btn-cancel']}>
-            취소
-          </button>
-          <button type="button" className={styles['btn-delete']}>
-            삭제
-          </button>
+export default function AlertModal({ type }) {
+  const AlertModalUI = {
+    'post-delete': (
+      <section className={styles.alert}>
+        <div className={styles['alert-inner']}>
+          <h2 className={styles['alert-title']}>상품을 삭제할까요?</h2>
+          <div className={styles['btn-wrapper']}>
+            <button type="button" className={styles['btn-cancel']}>
+              취소
+            </button>
+            <button type="button" className={styles['btn-delete']}>
+              삭제
+            </button>
+          </div>
         </div>
-      </div>
-    </section>
-  );
+      </section>
+    ),
+    logout: (
+      <section className={styles.alert}>
+        <div className={styles['alert-inner']}>
+          <h2 className={styles['alert-title']}>로그아웃하시겠어요?</h2>
+          <div className={styles['btn-wrapper']}>
+            <button type="button" className={styles['btn-cancel']}>
+              취소
+            </button>
+            <button type="button" className={styles['btn-logout']}>
+              로그아웃
+            </button>
+          </div>
+        </div>
+      </section>
+    ),
+  };
+  return AlertModalUI[type];
 }
