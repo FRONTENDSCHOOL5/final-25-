518345f41be9c33f2f37c757effe75ce9655952f
jongmi jung <whdal2636@naver.com>
2023-06-26 16:00:05 +0900

    [🎨 style] 회원가입 프로필설정 페이지 폰트 사이즈 및 간격 조정

--- src/pages/Join/JoinProfileSetting/JoinProfileSetting.jsx
+++ src/pages/Join/JoinProfileSetting/JoinProfileSetting.jsx
@@ -94,7 +94,7 @@ export default function JoinProfileSetting() {
   };
 
   return (
-    <main>
+    <section className={styles['join-profile-main']}>
       <h1 className={styles['header']}>프로필 설정</h1>
       <p className={`${styles['header']} ${styles['sub-title']}`}>
         나중에 언제든지 변경할 수 있습니다.
@@ -219,6 +219,6 @@ export default function JoinProfileSetting() {
           </button>
         </section>
       </form>
-    </main>
+    </section>
   );
 }
