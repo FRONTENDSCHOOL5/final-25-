ef6411d66923d247d44746a9b98dd8286f9fff06
jsunbin <jsnb6n@gmail.com>
2023-07-01 23:19:24 +0900

    [design] 검색 페이지 컴포넌트화 #178

--- src/components/common/User/UserSearch/UserSearch.jsx
+++ src/components/common/User/UserSearch/UserSearch.jsx
@@ -10,53 +10,21 @@ export default function UserSearch() {
    * - 사용자 이름과 계정을 검색할 수 있는 페이지입니다. 입력창에 텍스트를 입력하면 해당하는 사용자가 나오도록 합니다
    * - 검색어와 같은 단어에는 주황색 글씨가 표시됩니다.
    */
-  const token = localStorage.getItem('token');
   const username = 'lebao';
-  useEffect(() => {}, []);
 
   return (
     <>
-      <ul className={styles['user-list']}>
-        <li className={styles['user-item']}>
-          <Link to={`/profile/${username}`}>
-            <img
-              className={styles['profile-cover']}
-              src={defaultProfileImg}
-              alt="프로필 이미지"
-            />
-            <div className={styles['user-info']}>
-              <strong className={styles['user-name']}>러바오</strong>
-              <span className={styles['user-id']}>@ lebao</span>
-            </div>
-          </Link>
-        </li>
-        <li className={styles['user-item']}>
-          <Link to={`/profile/${username}`}>
-            <img
-              className={styles['profile-cover']}
-              src={defaultProfileImg}
-              alt="프로필 이미지"
-            />
-            <div className={styles['user-info']}>
-              <strong className={styles['user-name']}>러바오</strong>
-              <span className={styles['user-id']}>@ lebao</span>
-            </div>
-          </Link>
-        </li>
-        <li className={styles['user-item']}>
-          <Link to={`/profile/${username}`}>
-            <img
-              className={styles['profile-cover']}
-              src={defaultProfileImg}
-              alt="프로필 이미지"
-            />
-            <div className={styles['user-info']}>
-              <strong className={styles['user-name']}>러바오</strong>
-              <span className={styles['user-id']}>@ lebao</span>
-            </div>
-          </Link>
-        </li>
-      </ul>
+      <Link to={`/profile/${username}`} className={styles['user-to']}>
+        <img
+          className={styles['profile-cover']}
+          src={defaultProfileImg}
+          alt="프로필 이미지"
+        />
+        <div className={styles['user-info']}>
+          <strong className={styles['user-name']}>러바오</strong>
+          <span className={styles['user-id']}>@ lebao</span>
+        </div>
+      </Link>
     </>
   );
 }
--- src/pages/SearchUser/SearchUser.jsx
+++ src/pages/SearchUser/SearchUser.jsx
@@ -1,11 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
+import styles from './SearchUser.module.css';
 import UserSearch from '../../components/common/User/UserSearch/UserSearch';
+import { Link } from 'react-router-dom';
 import Layout from '../../components/layout/Layout';
+
 export default function SearchUser() {
+  const token = localStorage.getItem('token');
+
+  const [keyword, setKeyword] = useState('');
+
   return (
     <>
-      <Layout>
-        <UserSearch />
+      <Layout keyword={keyword}>
+        <ul className={styles['user-list']}>
+          <li className={styles['user-item']}>
+            <UserSearch />
+          </li>
+        </ul>
       </Layout>
     </>
   );
