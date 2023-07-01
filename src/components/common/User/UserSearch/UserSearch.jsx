c01cafb96c431d2399f54958902f50da68a2300b
jsunbin <jsnb6n@gmail.com>
2023-07-01 22:49:01 +0900

    [design] 유저 검색 UI 수정 #178

--- src/components/common/User/UserSearch/UserSearch.jsx
+++ src/components/common/User/UserSearch/UserSearch.jsx
@@ -1,86 +1,62 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import styles from '.././User.module.css';
 import defaultProfileImg from '../../../../assets/images/profile-img42.png';
+import { Link } from 'react-router-dom';
 
 export default function UserSearch() {
-  const chats = [
-    {
-      id: 1,
-      name: '냠냠이',
-    },
-    {
-      id: 2,
-      name: ' 쩝쩝이',
-    },
-    {
-      id: 3,
-      name: '호로록',
-    },
-    {
-      id: 4,
-      name: ' 통통이',
-    },
-  ];
+  /**
+   * **.2.4 검색**
+   * - 감귤마켓 피드 상단에 돋보기 버튼(검색 버튼)을 클릭하면 표시되는 페이지입니다
+   * - 사용자 이름과 계정을 검색할 수 있는 페이지입니다. 입력창에 텍스트를 입력하면 해당하는 사용자가 나오도록 합니다
+   * - 검색어와 같은 단어에는 주황색 글씨가 표시됩니다.
+   */
+  const token = localStorage.getItem('token');
+  const username = 'lebao';
+  useEffect(() => {}, []);
 
   return (
     <>
-      <div className={`${styles['user']} ${styles['user-search']}`}>
-        <h2 className="a11y-hidden">사용자 계정</h2>
-        <img
-          className={styles['profile-cover']}
-          src={defaultProfileImg}
-          alt="프로필 이미지"
-        />
-        <div className={styles['user-info']}>
-          <h3 className={styles['user-name']}>
-            <span>애월읍</span> {chats[0].name}
-          </h3>
-          <span className={styles['user-id']}>@ weniv_Mandarin</span>
-        </div>
-      </div>
-      <div className={styles['user']}>
-        <h2 className="a11y-hidden">사용자 계정</h2>
-        <img
-          className={styles['profile-cover']}
-          src={defaultProfileImg}
-          alt="프로필 이미지"
-        />
-        <div className={styles['user-info']}>
-          <h3 className={styles['user-name']}>
-            <span>애월읍</span> {chats[1].name}
-          </h3>
-          <span className={styles['user-id']}>@ weniv_Mandarin</span>
-        </div>
-      </div>
-      <div className={styles['user']}>
-        <h2 className="a11y-hidden">사용자 계정</h2>
-        <img
-          className={styles['profile-cover']}
-          src={defaultProfileImg}
-          alt="프로필 이미지"
-        />
-        <div className={styles['user-info']}>
-          <h3 className={styles['user-name']}>
-            <span>애월읍</span> {chats[2].name}
-          </h3>
-          <span className={styles['user-id']}>@ weniv_Mandarin</span>
-        </div>
-      </div>
-      <div className={styles['user']}>
-        <h2 className="a11y-hidden">사용자 계정</h2>
-        <img
-          className={styles['profile-cover']}
-          src={defaultProfileImg}
-          alt="프로필 이미지"
-        />
-        <div className={styles['user-info']}>
-          <h3 className={styles['user-name']}>
-            <span>애월읍</span>
-            {chats[3].name}
-          </h3>
-          <span className={styles['user-id']}>@ weniv_Mandarin</span>
-        </div>
-      </div>
+      <ul className={styles['user-list']}>
+        <li className={styles['user-item']}>
+          <Link to={`/profile/${username}`}>
+            <img
+              className={styles['profile-cover']}
+              src={defaultProfileImg}
+              alt="프로필 이미지"
+            />
+            <div className={styles['user-info']}>
+              <strong className={styles['user-name']}>러바오</strong>
+              <span className={styles['user-id']}>@ lebao</span>
+            </div>
+          </Link>
+        </li>
+        <li className={styles['user-item']}>
+          <Link to={`/profile/${username}`}>
+            <img
+              className={styles['profile-cover']}
+              src={defaultProfileImg}
+              alt="프로필 이미지"
+            />
+            <div className={styles['user-info']}>
+              <strong className={styles['user-name']}>러바오</strong>
+              <span className={styles['user-id']}>@ lebao</span>
+            </div>
+          </Link>
+        </li>
+        <li className={styles['user-item']}>
+          <Link to={`/profile/${username}`}>
+            <img
+              className={styles['profile-cover']}
+              src={defaultProfileImg}
+              alt="프로필 이미지"
+            />
+            <div className={styles['user-info']}>
+              <strong className={styles['user-name']}>러바오</strong>
+              <span className={styles['user-id']}>@ lebao</span>
+            </div>
+          </Link>
+        </li>
+      </ul>
     </>
   );
 }
