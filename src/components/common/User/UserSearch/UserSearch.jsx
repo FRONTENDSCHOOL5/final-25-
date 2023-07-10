1b35bb6b0c3d5150dc6c65ece47106769d082510
jsunbin <jsnb6n@gmail.com>
2023-07-10 16:58:50 +0900

    [refactor] 검색 페이지 컴포넌트 분리, 기능 개선 #184

--- src/components/common/User/UserSearch/UserSearch.jsx
+++ src/components/common/User/UserSearch/UserSearch.jsx
@@ -2,30 +2,46 @@ import React from 'react';
 import styles from './UserSearch.module.css';
 import { Link } from 'react-router-dom';
 
-export default function UserSearch({ key, data, keyword }) {
-  const rawUserName = data.username;
-
+function UserListItem({ item, keyword }) {
   return (
     <>
-      <Link to={`/profile/${data.accountname}`} className={styles['user-to']}>
+      <Link to={`/profile/${item.accountname}`} className={styles['user-to']}>
         <img
           className={styles['profile-cover']}
-          src={data.image}
-          alt={`${data.accountname} 프로필`}
+          src={item.image}
+          alt={`${item.accountname} 프로필`}
         />
         <div className={styles['user-info']}>
-          {rawUserName.includes(keyword) ? (
+          {item.username.includes(keyword) && keyword !== '' ? (
             <strong className={styles['user-name']}>
-              {rawUserName.split(keyword)[0]}
+              {item.username.split(keyword)[0]}
               <span className={styles.keyword}>{keyword}</span>
-              {rawUserName.slice(rawUserName.search(keyword) + keyword.length)}
+              {item.username.slice(
+                item.username.search(keyword) + keyword.length,
+              )}
             </strong>
           ) : (
-            <strong className={styles['user-name']}>{rawUserName}</strong>
+            <strong className={styles['user-name']}>{item.username}</strong>
           )}
-          <span className={styles['user-id']}>@ {data.accountname}</span>
+          <span className={styles['user-id']}>@ {item.accountname}</span>
         </div>
       </Link>
     </>
+  );
+}
+
+export default function UserList({ items, keyword }) {
+  const limitedItems = [...items].slice(0, 100);
+
+  return (
+    <ul className={styles['user-list']}>
+      {limitedItems.map(item => {
+        return (
+          <li key={item._id} className={styles['user-item']}>
+            <UserListItem item={item} keyword={keyword} />
+          </li>
+        );
+      })}
+    </ul>
   );
 }
