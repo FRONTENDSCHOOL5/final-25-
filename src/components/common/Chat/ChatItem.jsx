25b3e85886be997e85c1b95e2a1a83f056d64261
jongmi jung <whdal2636@naver.com>
2023-08-15 01:32:49 +0900

    [♻️ refactor] 필요없는 변수 및 주석 삭제

--- src/components/common/Chat/ChatItem.jsx
+++ src/components/common/Chat/ChatItem.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import styles from './ChatItem.module.css';
-import ProfileImg from '../../../assets/images/basic-profile-img.png';
 import { useNavigate } from 'react-router-dom';
 
 export default function ChatItem({ chat }) {
@@ -18,8 +17,6 @@ export default function ChatItem({ chat }) {
             src={chat.image}
             alt="프로필 이미지"
           />
-          {/*나중에 이즈메시지 동작되게끔 다시 확인 필요 */}
-          {/* {isNewMessage && <div className={styles.newMessageDot}></div>} */}
         </div>
         <div className={styles['chat-content']}>
           <h2 className={styles['chat-name']}>{chat.username}</h2>
